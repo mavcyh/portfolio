513bf97a9c1694fc545c242af9d9b793ceac0d40
Maverick Chin <64389689+mavcyh@users.noreply.github.com>
2025-01-07 00:20:45 +0800

    Created configuration file for GitHub actions and changed hosting location to /portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://mavcyh.github.io/portfolio',
+	base: '/portfolio',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
